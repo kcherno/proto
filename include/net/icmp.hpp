d143edf6c6eca0c9d05b3a34b5f2ad719aad2db6
Kirill Chernigovskiy <138433730+kcherno@users.noreply.github.com>
2025-02-02 16:48:39 +0800

    Add: icmp packet interface

---
+++ include/net/icmp.hpp
@@ -0,0 +1,65 @@
+#ifndef NET_ICMP_HPP__
+#define NET_ICMP_HPP__
+
+#include <cstdint>
+
+#include "tool/mutable_buffer.hpp"
+
+namespace proto::net
+{
+    class icmp {
+    public:
+	constexpr icmp() noexcept :
+	    icmp {0, 0}
+	{}
+
+	constexpr icmp(uint8_t type, uint8_t code) noexcept :
+	    type_     {type},
+	    code_     {code},
+	    checksum_ {0}
+	{}
+
+	constexpr uint8_t type() const noexcept
+	{
+	    return type_;
+	}
+
+	constexpr void type(uint8_t other) noexcept
+	{
+	    type_ = other;
+	}
+
+	constexpr uint8_t code() const noexcept
+	{
+	    return code_;
+	}
+
+	constexpr void code(uint8_t other) noexcept
+	{
+	    code_ = other;
+	}
+
+	void calculate_checksum(std::size_t) noexcept;
+
+	consteval static std::size_t header_length() noexcept
+	{
+	    return sizeof(icmp);
+	}
+
+    private:
+	uint8_t  type_;
+	uint8_t  code_;
+	uint16_t checksum_;
+    };
+
+    std::size_t make_icmp_header(void*, std::size_t, uint8_t, uint8_t) noexcept;
+
+    inline std::size_t make_icmp_header(tool::mutable_buffer buffer,
+					uint8_t              type,
+					uint8_t              code) noexcept
+    {
+	return make_icmp_header(buffer.data(), buffer.size(), type, code);
+    }
+}
+
+#endif // NET_ICMP_HPP__
---
+++ src/net/icmp.cpp
@@ -0,0 +1,51 @@
+#include <cstdint>
+
+#include "net/icmp.hpp"
+
+namespace proto::net
+{
+    void icmp::calculate_checksum(std::size_t size) noexcept
+    {
+	checksum_ = 0;
+
+	uint32_t sum = 0;
+
+	auto pointer = reinterpret_cast<const uint16_t*>(this);
+
+	for (; size; size -= 2)
+	{
+	    sum += *pointer++;
+	}
+
+	if (size == 1)
+	{
+	    sum += *reinterpret_cast<const uint8_t*>(pointer);
+	}
+
+	while (sum >> 16)
+	{
+	    sum += (sum >> 16) + (sum & 0xFFFF);
+	}
+
+	checksum_ = static_cast<uint16_t>(~sum);
+    }
+
+    std::size_t make_icmp_header(void*       data,
+				 std::size_t size,
+				 uint8_t     type,
+				 uint8_t     code) noexcept
+    {
+	if (size < icmp::header_length())
+	{
+	    return 0;
+	}
+
+	auto pointer = reinterpret_cast<icmp*>(data);
+
+	pointer->type(type);
+	pointer->code(code);
+	pointer->calculate_checksum(size);
+
+	return size;
+    }
+}
